abd217d22637b67d546c49e18ff1a4fc54afe0a1
user <user@user-PC>
2016-09-27 13:48:53 +0530

    userPermission

---
+++ src/view/MyDb.java
@@ -0,0 +1,31 @@
+package view;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+public class MyDb {
+    public static Connection con;
+    
+    public static void getConnection() throws Exception
+    {
+        Class.forName("com.mysql.jdbc.Driver");
+        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/tharaka", "root", "1234");
+    }
+    public static void set(String sql) throws Exception
+    {   
+            if(con==null)
+            {
+            getConnection();
+            }
+            con.createStatement().execute(sql);
+    }
+    public static ResultSet search(String sql) throws Exception
+    {
+        if(con==null)
+        {
+          getConnection();
+        }
+        return con.createStatement().executeQuery(sql);
+    }
+}
